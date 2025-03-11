23f4988bc88b03c367771c13126408aaebcea0d8
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-03-11 16:32:00 +0900

    feat: Bottom Navigation Icon 변경

--- app/src/main/java/com/kuit/kuit5/MainActivity.kt
+++ app/src/main/java/com/kuit/kuit5/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
@@ -18,8 +19,11 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.kuit.kuit5.navigation.BottomNavItem
 import com.kuit.kuit5.navigation.KuitNavGraph
@@ -38,32 +42,32 @@ class MainActivity : ComponentActivity() {
                     BottomNavItem(
                         label = "홈",
                         route = Route.Home.route,
-                        selectedIcon = R.drawable.ic_home,
-                        unselectedIcon = R.drawable.ic_x
+                        selectedIcon = R.drawable.ic_bottomnav_home,
+                        unselectedIcon = R.drawable.ic_bottomnav_home
                     ),
                     BottomNavItem(
                         label = "자산",
                         route = Route.Assets.route,
-                        selectedIcon = R.drawable.ic_home,
-                        unselectedIcon = R.drawable.ic_x
+                        selectedIcon = R.drawable.ic_bottomnav_asset,
+                        unselectedIcon = R.drawable.ic_bottomnav_asset
                     ),
                     BottomNavItem(
                         label = "가계부",
                         route = Route.Records.route,
-                        selectedIcon = R.drawable.ic_home,
-                        unselectedIcon = R.drawable.ic_x
+                        selectedIcon = R.drawable.ic_bottomnav_records,
+                        unselectedIcon = R.drawable.ic_bottomnav_records
                     ),
                     BottomNavItem(
                         label = "건강",
                         route = Route.Health.route,
-                        selectedIcon = R.drawable.ic_home,
-                        unselectedIcon = R.drawable.ic_x
+                        selectedIcon = R.drawable.ic_bottomnav_health,
+                        unselectedIcon = R.drawable.ic_bottomnav_health
                     ),
                     BottomNavItem(
                         label = "금융쇼핑",
                         route = Route.Shopping.route,
-                        selectedIcon = R.drawable.ic_home,
-                        unselectedIcon = R.drawable.ic_x
+                        selectedIcon = R.drawable.ic_bottomnav_shopping,
+                        unselectedIcon = R.drawable.ic_bottomnav_shopping
                     )
                 )
 
@@ -74,6 +78,16 @@ class MainActivity : ComponentActivity() {
                     contentWindowInsets = WindowInsets.safeDrawing,
                     bottomBar = {
                         NavigationBar(
+                            modifier = Modifier
+                                .drawBehind {
+                                    val strokeWidth = 1.dp.toPx()
+                                    drawLine(
+                                        color = Color(0xFFD9D9D9), // NavigationBar의 상단 테두리
+                                        start = Offset(0f, 0f),
+                                        end = Offset(size.width, 0f),
+                                        strokeWidth = strokeWidth,
+                                    )
+                                },
                             containerColor = Color.White,
                         ) {
                             navBarItems.forEachIndexed { index, item ->
@@ -83,9 +97,6 @@ class MainActivity : ComponentActivity() {
                                     label = {
                                         Text(
                                             text = item.label,
-                                            color = if (index == selectedIndex){
-                                                Color.Black
-                                            } else Color.Gray
                                         )
                                     },
                                     onClick = {
@@ -99,9 +110,16 @@ class MainActivity : ComponentActivity() {
                                                     item.selectedIcon
                                                 } else item.unselectedIcon
                                             ),
-                                            contentDescription = item.label
+                                            contentDescription = item.label,
                                         )
-                                    }
+                                    },
+                                    colors = NavigationBarItemDefaults.colors(
+                                        indicatorColor = Color.Transparent,
+                                        selectedIconColor = Color.Black,
+                                        unselectedIconColor = Color(0xFF9FA5B0),
+                                        selectedTextColor = Color.Black,
+                                        unselectedTextColor = Color(0xFF9FA5B0)
+                                    )
                                 )
                             }
                         }
